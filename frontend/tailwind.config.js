d0a40162224e0e2b9b5976f9232bd3bcd31b91a0
Marcell Andreas Samadhani Duha <marcellandreas.123@gmail.com>
2025-06-03 19:15:33 +0700

    settings initials fe

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,34 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "edf1c9",
+        secondary: "#8ac243",
+        tertiary: "#222",
+        secondaryRed: "#f42c37",
+        secondaryYellow: "#fdc62c",
+        secondaryGreen: "#2dcc6f",
+        secondaryBlue: "#1376f4",
+        secondaryWhite: "#eee",
+        gray: {
+          10: "#eee",
+          20: "#a2a2a2",
+          30: "7b7b7b",
+          40: "585858",
+          90: "#141414",
+        },
+      },
+      screens: {
+        xs: "400px",
+        "3xl": "1680px",
+        "4xl": "2200px",
+      },
+      backgroundImage: {
+        hero: "url(/src/assets/bg.png)",
+      },
+    },
+  },
+  plugins: [],
+};
