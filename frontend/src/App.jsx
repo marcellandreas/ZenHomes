928f89de9380b9df7575672b7ac664591aea70bd
Marcell Andreas Samadhani Duha <marcellandreas.123@gmail.com>
2025-06-19 01:06:15 +0700

    feat: pages routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,28 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import React, { Suspense } from "react";
 import Home from "./pages/home";
-import Header from "./components/Header";
-import Hero from "./components/Hero";
-import Footer from "./components/Footer";
+
+import Layout from "./components/Layout";
+import Listing from "./pages/Listing";
+import Bookings from "./pages/Bookings";
+import Favourites from "./pages/Favourites";
+import Property from "./pages/Property";
 const App = () => {
   return (
     <BrowserRouter>
-      <Header />
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
-      <Footer />
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route element={<Layout />}>
+            <Route path="/" element={<Home />} />
+            <Route path="/listing">
+              <Route index element={<Listing />} />
+              <Route index path=":propertyId" element={<Property />} />
+            </Route>
+            <Route path="/bookings" element={<Bookings />} />
+            <Route path="/favourites" element={<Favourites />} />
+          </Route>
+        </Routes>
+      </Suspense>
     </BrowserRouter>
   );
 };
