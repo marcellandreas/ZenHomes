9de4582514b294caa011f099512c3ab51d5c1f57
Marcell Andreas Samadhani Duha <marcellandreas.123@gmail.com>
2025-06-30 23:37:49 +0700

    Fix import path case for Home.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import React, { Suspense, useEffect, useState } from "react";
-import Home from "./pages/home";
+import Home from "./pages/Home";
 
 import Layout from "./components/Layout";
 import Listing from "./pages/Listing";
